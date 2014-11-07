462c34efdb48684f0eb923109a1615caac306fcb
pdigiglio <p.digiglio91@gmail.com>
2014-11-07 10:56:09 +0100

    sweep code_analysis and code_analysis_fixed

--- tutorial_4/code_analysis.cpp
+++ tutorial_4/code_analysis.cpp
@@ -22,20 +22,19 @@
 
 	int
 main ( int argc, char *argv[] ) {
-	int xvalues[128];
+	int xvalues[10];
 
 	// XXX problem with space allocation of your array
 
 	// XXX (signed) char are from -128 to 127 so I have an infinite loop
-	for( int i = 0; i < 128; i ++ ) {
+	for( char i = 0 i < 128; i ++ ) {
 		// you cannot use a char into an array index 
 		// in C++ (maybe an unsigned char works)
-		xvalues[ i ];
+		std::cin >> xvalues[ static_cast<int>( i ) ];
 	}
 
 	// this value is 0 because you firs perform the operation as integer
-	//
-	double a = 1.0 / 3.0;
+	double a = static_cast<double>( 1 / 3 );
 	double b = 5;
 	double c = 10;
 
@@ -43,7 +42,7 @@ main ( int argc, char *argv[] ) {
 		int x = xvalues[ i ];
 		// XXX ^ is a XOR binary operator
 		// XXX 1 / a is an error because of 1 / 0
-		double result = ( 1 / a ) * ( ( x + 1 ) * ( x + 1 ) ) + b * x + c;
+		double result = ( 1 / a ) * ( ( x + 1 ) ^ 2 ) + b * x + c;
 		std::cout << "x = " << xvalues[ i ]
 			<< ", result = " << result << std::endl;
 	}
