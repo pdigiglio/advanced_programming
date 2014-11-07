a70462e49f1ecda6270a226c1a09e6ea7898a9bb
pdigiglio <p.digiglio91@gmail.com>
2014-11-07 14:27:41 +0100

    add source to evaluate \pi using a Montecarlo method

---
+++ tutorial_4/pi_approximation.cpp
@@ -0,0 +1,58 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  pi_approximation.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/07/2014 01:29:12 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  P. Di Giglio (github.com/pdigiglio), p.digiglio91@gmail.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+
+	int
+main ( int argc, char *argv[] ) {
+	// random seed
+	srand( time( NULL ) );
+	
+	// variable for timing
+	clock_t start = clock();
+	
+	// number of throws
+	unsigned int throws = 100000000;
+
+	// coordinates of the point
+	double x, y;
+	// counter for points inside the circle
+	unsigned int inside = 0;
+	for ( unsigned int j = 0; j < throws; ++ j ) {
+		// choose two random numbers with uniform distribution
+		x = (double) rand() / RAND_MAX;
+		y = (double) rand() / RAND_MAX;
+
+		// checks if the point is in the circle
+		if( y * y < 1.0 - x * x )
+			inside ++;
+	}
+
+	// I take 0,1 as random variable so that <x^2> = <x>
+	double piFour = ( (double) inside / throws ); 
+	std::cout << piFour * 4 << " +/- " << 
+		4 * sqrt( piFour * ( 1 - piFour ) / ( throws - 1 ) )  << std::endl;
+
+	// to see the difference in time with optimization options
+	std::cout << "Time: " << (double) ( clock() - start ) / CLOCKS_PER_SEC << std::endl;
+	return 0;
+}				/* ----------  end of function main  ---------- */
