55b3ce67334fee5b71034d184785e06f398c58dd
pdigiglio <p.digiglio91@gmail.com>
2014-12-07 21:46:46 +0100

    add solution about 'Type Casting' assignment

---
+++ tutorial_9/type_casts.cpp
@@ -0,0 +1,52 @@
+/**
+ *
+ *
+ *           @name  type_casts.cpp
+ *
+ *        @version  1.0
+ *           @date  12/07/2014 (07:15:21 PM)
+ *       @revision  none
+ *       @compiler  gcc
+ *
+ *
+ *         @author  P. Di Giglio (github.com/pdigiglio), <p.digiglio91@gmail.com>
+ *        @company  
+ *          @brief  
+ *
+ *          Example usage:
+ *          @code
+ *          @endcode
+ *
+ *
+ */
+
+#include <iostream>
+
+class Base {
+	public:
+		void echo ( void ) {
+			std::cout << "Base" << std::endl;
+		};
+};
+
+class Derived: public Base {
+	public:
+		void echo ( void ) {
+			std::cout << "Derived" << std::endl;
+		};
+};
+
+	int
+main ( void ) {
+
+	Base* ptr = new Derived();
+	/// This will call Derived::echo() if Base::echo() is declared to be `virtual`.
+	ptr->echo();
+
+	/// If you use this `stack` allocation, Derived::echo() will be called even if
+	/// the method is not virtual.
+//	Derived derived;
+//	derived.echo();
+
+	return 0;
+}
