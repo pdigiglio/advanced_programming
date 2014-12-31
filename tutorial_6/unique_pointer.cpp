a5dc86b8e5f7f4a32613ed8764fc447f1983f4eb
pdigiglio <p.digiglio91@gmail.com>
2014-12-31 19:47:50 +0100

    add a solution with helpful comments

--- tutorial_6/unique_pointer.cpp
+++ tutorial_6/unique_pointer.cpp
@@ -39,7 +39,8 @@ public:
 	/// This function just assigns the passed `T *obj` pointer to the local `T* _obj`.
 	/// Then assigns to `_index` the value `Index` and _then_ increases the latter by 1.
     UniquePointer( T* obj ) : _obj( obj ), _index( Index++ ) {
-		std::cerr << "constructor" << std::endl;
+		std::cerr << " > constructor: _index = " << _index <<
+			" Index = " << Index << std::endl;
 	};
 
     // TODO: complete the implementation, implement the _semantics_ of a unique pointer!
@@ -48,7 +49,10 @@ public:
 	/// 
 	/// "move" = copies the address of the old `_obj` and nullifies it.
     UniquePointer( UniquePointer& uptr ) : _obj( uptr._obj ),  _index(Index++) {
-		std::cerr << "copy constructor" << std::endl;
+		std::cerr << " > copy constructor: _index = " << _index <<
+			" Index = " << Index << std::endl;
+
+		// nullify the objet I copiated from
 		uptr._obj = nullptr;
 	};
 
@@ -58,9 +62,9 @@ public:
 	/// the passed argument to be edited (and hence his `._obj` field to be nullified).
     UniquePointer( const UniquePointer& uptr ) = delete;
 
-    /// destructor that cleans up if necessary, also notifies about actions taken
+    /// @brief destructor that cleans up if necessary, also notifies about actions taken
     ~UniquePointer() {
-        std::cout << "Deleting index " << _index << " of class "
+        std::cout << "Deleting _index " << _index << " of class "
 			<< typeid(_obj).name() << std::endl;
 
     	// TODO complete the implementation
@@ -74,14 +78,18 @@ public:
             std::cout << "-> object already destroyed" << std::endl;
         }
 
-		/// @attention When you delete this object, his destructor will be called!
-		/// So if two instances of `UniquePointer` point to the same object (?)
+		// @attention When you delete this object, his destructor will be called!
+		// So if two instances of `UniquePointer` point to the same object (?)
 //		delete _obj;
-		std::cout << "_obj value " << _obj << std::endl;
+//		std::cout << "_obj (address) value " << _obj << std::endl;
     };
 
     /// assignment operator for unique pointer type 
     // TODO: is it needed? is it clean?
+	
+	/// @brief Assignment operator
+	///
+	/// This doesn't increase neither `_index` or `Index`.
     void
     operator=(UniquePointer& uptr) // = delete;
     {
@@ -89,10 +97,19 @@ public:
 
 		std::cerr << "assignment operator" << std::endl;
 
-		/// Notify the user he's deleting an existing object.
-		if ( _obj != nullptr ) 
+		/// Notify the user he's deleting an existing object. This will also notify
+		/// if the current `_obj` has been deleted since after `delete _obj` his address
+		/// is _not_ set to `nullptr`. 
+		if ( _obj != nullptr )
+		{
 			std::cerr << "Index " << Index << " of class " << typeid( _obj ).name() << 
 				" >>> Deleting object upon new assignment!" << std::endl;
+		}
+		else
+		{
+			std::cerr << "Index " << Index << " of class " << typeid( _obj ).name() << 
+				" >>> Inserting object upon new assignment!" << std::endl;
+		}
 
 		/// Copy the object.
 		_obj = uptr._obj;
@@ -177,12 +194,11 @@ int main() {
 			// now the `_obj` field of `uptr1` is 0
 //			uptr1.printAddress();
 
-			uptr2.printAddress();
+//			uptr2.printAddress();
 
-			// define a constant reference
             const PtrType& uptr2_cref = uptr2;
-			uptr2.printAddress();
-			uptr2_cref.printAddress();
+//			uptr2.printAddress();
+//			uptr2_cref.printAddress();
 
 			// After this assignment `uptr2._obj` is equal to `nullptr` so it will not be
 			// destroyed because of the test.
@@ -192,17 +208,18 @@ int main() {
 			uptr2_cref->doSomething();
         }
     }
-/*
-    std::cout << "test case 2: deleting in outer context" << std::endl;
+
+    std::cout << std::endl << "test case 2: deleting in outer context" << std::endl;
     {
         PtrType uptr3(nullptr);
         {
             PtrType uptr4(new TestClass());
             // TODO forbid the assignment operator and think about how this case can still be made work
+
+			// Now `uptr3` contains the address of `TestClass()` and `uptr4._obj = nullptr`
             uptr3 = uptr4;
         }
     }
-*/
 
     return 0;
 }
